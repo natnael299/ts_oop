d4b6f92e074bed20644683b62476b9ebe2d06eb5
natnael <natibeyene49@gmail.com>
2026-08-18 14:06:05 +0300

    molde the shapes to one canvas

--- src/shapes-demo.ts
+++ src/shapes-demo.ts
@@ -1,15 +1,16 @@
 export {};
 
+const canvasEle = document.getElementById("mycanvas") as HTMLCanvasElement;
+const ctx = canvasEle.getContext("2d") as CanvasRenderingContext2D;
+
 // draw a rectangele
 const rectangleProps = {
-  canvasEle: document.getElementById("mycanvas") as HTMLCanvasElement,
-  x: 500,
-  y: 150,
+  x: 100,
+  y: 50,
   width: 300,
   height: 200,
   color: "red",
   drawRectangle: function () {
-    const ctx = this.canvasEle.getContext("2d") as CanvasRenderingContext2D;
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   },
@@ -19,18 +20,14 @@ rectangleProps.drawRectangle();
 
 // draw a circle
 const circle = {
-  circleEle: document.getElementById("circle") as HTMLCanvasElement,
-  x: 200,
+  x: 150,
   y: 150,
   radius: 50,
   drawCircle: function () {
-    const circleCtx = this.circleEle.getContext(
-      "2d",
-    ) as CanvasRenderingContext2D;
-    circleCtx.beginPath();
-    circleCtx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    circleCtx.fillStyle = "deepskyblue";
-    circleCtx.fill();
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "deepskyblue";
+    ctx.fill();
   },
 };
 
